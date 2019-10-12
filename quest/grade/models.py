e54468ed5ee6ff0d070d891cb1402358059a7bdb
Shunsuke Nishigami <sgami2424@gmail.com>
2019-10-12 18:59:53 +0900

    feat: create grade model

--- quest/grade/models.py
+++ quest/grade/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Grade(models.Model):
+    english = models.IntegerField()
+    math = models.IntegerField()
+    japanese = models.IntegerField()
+    gpa = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
--- quest/grade/views.py
+++ quest/grade/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the grade index.")
-
-
-# Create your views here.
+    return render(request, "grade/index.html")
