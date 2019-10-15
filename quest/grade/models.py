8f1cd8ca9d0d317348972721324594fbeffdbd13
wafuwafu13 <h.tagawa.miraidenshi@gmail.com>
2019-10-15 13:09:10 +0900

    feat:create_board_model

--- quest/grade/models.py
+++ quest/grade/models.py
@@ -11,3 +11,11 @@ class Grade(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Board(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=200)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.user.username
