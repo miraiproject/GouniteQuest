34b832067c8d35b4c23b3a2f645d4f76583ae94a
wafuwafu13 <h.tagawa.miraidenshi@gmail.com>
2019-10-15 13:24:39 +0900

    feat:create_board_form

--- quest/grade/forms.py
+++ quest/grade/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
-from .models import Grade
+from .models import Grade, Board
+
 
 
 class GradeForm(ModelForm):
     class Meta:
         model = Grade
         fields = ['english', 'math', 'japanese']
+
+class BoardForm(ModelForm):
+    class Meta:
+        model = Board
+        fields = ['comment', 'date']
