05413e4a9f5afb2bab31a235247e1aaa8e53c74c
Shunsuke Nishigami <sgami2424@gmail.com>
2019-10-26 15:13:29 +0900

    feat: add tests to IndexTests and NewGradeTest

--- quest/grade/tests.py
+++ quest/grade/tests.py
@@ -3,7 +3,8 @@ from django.test import Client, TestCase
 from .views import signup, index
 from .views_grade import new_grade, update_grade, show_grade
 from .views_report import new_report_problem, update_report_problem, new_report
-from .models import CustomUser, Grade, ReportProblem
+from .models import CustomUser, Grade, ReportProblem, Report
+from .forms import GradeForm
 
 
 def create_client_and_teacher_and_student(self):
@@ -71,6 +72,43 @@ class IndexTests(TestCase):
         view = resolve('/')
         self.assertEqual(view.func, index)
 
+    def test_context(self):
+        url = reverse('grade:index')
+
+        self.client.force_login(self.student)
+        response = self.client.get(url)
+        self.assertEqual(None, response.context['grade'])
+        self.assertEqual(0, response.context['reports'].count())
+        self.assertEqual(0, response.context['submitted_problems'].count())
+        self.assertEqual(0, response.context['not_submitted_problems'].count())
+
+        grade = Grade.objects.create(
+            english=21, math=32, japanese=7, gpa=20, user=self.student
+        )
+        report_problem = ReportProblem.objects.create(
+            title="title", content="content", teacher=self.teacher
+        )
+
+        response = self.client.get(url)
+        self.assertEqual(grade, response.context['grade'])
+        self.assertEqual(1, response.context['not_submitted_problems'].count())
+
+        Report.objects.create(
+            report_file="test.jpg",
+            report_problem=report_problem,
+            student=self.student
+        )
+
+        response = self.client.get(url)
+        self.assertEqual(1, response.context['reports'].count())
+        self.assertEqual(1, response.context['submitted_problems'].count())
+        self.assertEqual(0, response.context['not_submitted_problems'].count())
+
+        self.client.force_login(self.teacher)
+        response = self.client.get(url)
+        self.assertEqual(1, response.context['report_problems'].count())
+        self.assertEqual(1, response.context['reports'].count())
+
 
 class NewGradeTests(TestCase):
 
@@ -84,6 +122,25 @@ class NewGradeTests(TestCase):
     def test_new_grade_url_resolves_new_grade_view(self):
         view = resolve('/new_grade/%d/' % self.student.id)
         self.assertEqual(view.func, new_grade)
+
+    def test_context(self):
+        url = reverse('grade:new_grade', args=[self.student.id])
+
+        self.client.force_login(self.teacher)
+        response = self.client.get(url)
+        self.assertEqual(self.student, response.context['user'])
+
+    def test_grade_form(self):
+        url = reverse('grade:new_grade', args=[self.student.id])
+        form_data = {'english': 21, 'math': 32, 'japanese': 13}
+        form = GradeForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        self.client.force_login(self.teacher)
+        self.assertEqual(0, Grade.objects.all().count())
+        response = self.client.post(url, form_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(1, Grade.objects.all().count())
+        self.assertEqual(22.0, Grade.objects.all().first().gpa)
 
 
 class UpdateGradeTests(TestCase):
